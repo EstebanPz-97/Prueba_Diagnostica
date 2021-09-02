26d62d701ae90f49a3b3360e4b53d65cfbed50cb
esteban perez <esteban.perez@alteru.co>
2021-09-02 08:27:05 -0500

    arregalmo la prueba

--- src/components/Preguntas/index.js
+++ src/components/Preguntas/index.js
@@ -5,8 +5,7 @@ import { Radio, RadioGroup,FormControlLabel,FormControl,FormLabel,Button } from 
 export const Preguntas = () => {
     const [value, setValue] = useState("");
     const [error, setError]= useState(false);
-    const [count ,setConunt] = useState(0)
-
+    const [count ,setCount] = useState(0)
     const handleChange = (event) => {
       setValue(event.target.value);
       setError(false);  
@@ -14,10 +13,20 @@ export const Preguntas = () => {
     const handleSubmit = (event) =>{
       event.preventDefault();
       if (value === "true"){
-        setConunt (count + 1 );
+        setCount (count + 1 );
         setError(false);
-      }else{
-        setConunt (count + 0 );
+      }else if (value === "true2"){
+        setCount (count + 1 );
+        setError(false);
+      }else if (value === "true3"){
+        setCount (count + 1 );
+        setError(false);
+      }else if (value === "true4"){
+        setCount (count + 1 );
+        setError(false);
+      }
+      else  {
+        setCount (count + 0 );
         setError(true);
       }
       console.log(count);
@@ -27,6 +36,7 @@ export const Preguntas = () => {
      return (
          <div className="container-quiz">
           <form onSubmit={handleSubmit}> 
+          <h1>Score {count}</h1>
            <FormControl component="fieldset">
      <li> <FormLabel component="legend">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum perferendis consequuntur molestias numquam, dolore facilis corrupti ea voluptas quaerat voluptate assumenda laborum quas illo nisi culpa, distinctio aperiam dolor veritatis?</FormLabel>
       <RadioGroup aria-label="gender" name="gender1" value={value} onChange={handleChange}>
@@ -37,7 +47,7 @@ export const Preguntas = () => {
       </li>
       <li> <FormLabel component="legend">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum perferendis consequuntur molestias numquam, dolore facilis corrupti ea voluptas quaerat voluptate assumenda laborum quas illo nisi culpa, distinctio aperiam dolor veritatis?</FormLabel>
       <RadioGroup aria-label="gender" name="gender1" value={value} onChange={handleChange}>
-        <FormControlLabel value="true" control={<Radio />} label="lorem" />
+        <FormControlLabel value="true2" control={<Radio />} label="lorem" />
         <FormControlLabel value="false" control={<Radio />} label="lorem" />
         <FormControlLabel value="false" control={<Radio />} label="lorem" />
       </RadioGroup>
@@ -45,12 +55,12 @@ export const Preguntas = () => {
       <RadioGroup aria-label="gender" name="gender1" value={value} onChange={handleChange}>
         <FormControlLabel value="false" control={<Radio />} label="lorem" />
         <FormControlLabel value="false" control={<Radio />} label="lorem" />
-        <FormControlLabel value="true" control={<Radio />} label="lorem" />
+        <FormControlLabel value="true3" control={<Radio />} label="lorem" />
       </RadioGroup>
       </li> <li> <FormLabel component="legend">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum perferendis consequuntur molestias numquam, dolore facilis corrupti ea voluptas quaerat voluptate assumenda laborum quas illo nisi culpa, distinctio aperiam dolor veritatis?</FormLabel>
       <RadioGroup aria-label="gender" name="gender1" value={value} onChange={handleChange}>
         <FormControlLabel value="false" control={<Radio />} label="lorem" />
-        <FormControlLabel value="true" control={<Radio />} label="lorem" />
+        <FormControlLabel value="true4" control={<Radio />} label="lorem" />
         <FormControlLabel value="false" control={<Radio />} label="lorem" />
       </RadioGroup>
       </li>

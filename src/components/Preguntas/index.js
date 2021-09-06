e7ecb7738e77e87d0348ba1a852ac3eef00f127d
esteban perez <esteban.perez@alteru.co>
2021-09-06 12:51:53 -0500

    conexion base de datos prueba

--- src/components/Preguntas/index.js
+++ src/components/Preguntas/index.js
@@ -31,7 +31,26 @@ const useStyles = makeStyles((theme) => ({
 export const Preguntas = () => {
   const [open, setOpen] = useState(false);
   const [value, setValue] = useState("");
-  const [count ,setCount] = useState(0)
+  const [count ,setCount] = useState(0);
+
+  const formik = useFormik({
+    initialValues:{
+      1:{
+        rta1:'',
+      },
+      2:{
+        rta2:'',
+      },
+      3:{
+        rta3:'',
+      },
+      4:{
+        rta4:''
+      },
+      score:0,
+    } 
+  });
+
   const handleChange = (event) => {
     setValue(event.target.value);
       // setError(false);  
@@ -40,116 +59,92 @@ export const Preguntas = () => {
     {
       label:<FormLabel component="legend" name="pregunta">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum perferendis consequuntur molestias numquam, dolore facilis corrupti ea voluptas quaerat voluptate assumenda laborum quas illo nisi culpa, distinctio aperiam dolor veritatis?</FormLabel>,
       content:
+    
       <RadioGroup aria-label="gender" name="gender1" value={value} onChange={handleChange}>
-      <FormControlLabel value="option1.1" name="rta" control={<Radio />} label="lorem" />
-      <FormControlLabel value="true1" name="rta" control={<Radio />} label="lorem" />
-      <FormControlLabel value="option1.2" name="rta" control={<Radio />} label="lorem" />
+      <FormControlLabel value="option1.1" onChange={formik.handleChange}  name="1.rta1" control={<Radio />} label="lorem" />
+      <FormControlLabel value="true1" onChange={formik.handleChange}  name="1.rta1" control={<Radio  />} label="lorem" />
+      <FormControlLabel value="option1.2" onChange={formik.handleChange}  name="1.rta1" control={<Radio  />} label="lorem" />
     </RadioGroup>   ,
     },
     {
       label:<FormLabel component="legend">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum perferendis consequuntur molestias numquam, dolore facilis corrupti ea voluptas quaerat voluptate assumenda laborum quas illo nisi culpa, distinctio aperiam dolor veritatis?</FormLabel>,
       content:
       <RadioGroup aria-label="gender" name="gender1" value={value} onChange={handleChange}>
-      <FormControlLabel value="true2" control={<Radio />} label="lorem" />
-      <FormControlLabel value="option1.2" control={<Radio />} label="lorem" />
-      <FormControlLabel value="option2.2" control={<Radio />} label="lorem" />
+      <FormControlLabel value="true2" onChange={formik.handleChange}  name="2.rta2" control={<Radio />} label="lorem" />
+      <FormControlLabel value="option1.2" onChange={formik.handleChange}  name="2.rta2" control={<Radio />} label="lorem" />
+      <FormControlLabel value="option2.2" onChange={formik.handleChange}  name="2.rta2" control={<Radio />} label="lorem" />
     </RadioGroup>   ,
     },
     {
       label:<FormLabel component="legend">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum perferendis consequuntur molestias numquam, dolore facilis corrupti ea voluptas quaerat voluptate assumenda laborum quas illo nisi culpa, distinctio aperiam dolor veritatis?</FormLabel>,
       content:
       <RadioGroup aria-label="gender" name="gender1" value={value} onChange={handleChange}>
-      <FormControlLabel value="option3.1" control={<Radio />} label="lorem" />
-      <FormControlLabel value="option3.2" control={<Radio />} label="lorem" />
-      <FormControlLabel value="true3" control={<Radio />} label="lorem" />
+      <FormControlLabel value="option3.1" onChange={formik.handleChange}  name="3.rta3" control={<Radio />} label="lorem" />
+      <FormControlLabel value="option3.2" onChange={formik.handleChange}  name="3.rta3" control={<Radio />} label="lorem" />
+      <FormControlLabel value="true3" onChange={formik.handleChange}  name="3.rta3" control={<Radio />} label="lorem" />
     </RadioGroup>,
     },
     {
       label:<FormLabel component="legend">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum perferendis consequuntur molestias numquam, dolore facilis corrupti ea voluptas quaerat voluptate assumenda laborum quas illo nisi culpa, distinctio aperiam dolor veritatis?</FormLabel>,
       content:
       <RadioGroup aria-label="gender" name="gender1" value={value} onChange={handleChange}>
-      <FormControlLabel value="option4.1" control={<Radio />} label="lorem" />
-      <FormControlLabel value="true4" control={<Radio />} label="lorem" />
-      <FormControlLabel value="option4.2" control={<Radio />} label="lorem" />
+      <FormControlLabel value="option4.1" onChange={formik.handleChange}  name="4.rta4" control={<Radio />} label="lorem" />
+      <FormControlLabel value="true4" onChange={formik.handleChange}  name="4.rta4" control={<Radio />} label="lorem" />
+      <FormControlLabel value="option4.2" onChange={formik.handleChange}  name="4.rta4" control={<Radio />} label="lorem" />
       <Button
         disabled={open}
         variant="outlined"
-        onClick={() => {
-          setOpen(true);
-        }}
-      >
+        onClick={handleSubmit}>
         Terminado
       </Button>
     </RadioGroup>
 
     ,
-    },
-  ];
-  const classes = useStyles();
-  const theme = useTheme();
-  const [activeStep, setActiveStep] = useState(0);
-  const maxSteps = tutorialSteps.length;
-  
-  const handleNext = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
-    if (value === "true1"){
-      setCount (count + 1 );
-    } else if (value === "true2") {
-      setCount (count + 1 );
-    }else if (value === "true3"){
-      setCount(count + 1)
-    }else if (value === "true4"){
-      setCount (count +1)
-    }else{
-      setCount (count + 0)
-    }
-  };
-  
-  const handleBack = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep - 1);
-  };
-  const formik = useFormik({
-    initialValues:{
-      Pregunta:'',
-      rta:''
-    },
-    onSubmit: values =>{
-      setActiveStep((prevActiveStep) => prevActiveStep + 1);
-    if (value === "true1"){
-      setCount (count + 1 );
-    } else if (value === "true2") {
-      setCount (count + 1 );
-    }else if (value === "true3"){
-      setCount(count + 1)
-    }else if (value === "true4"){
-      setCount (count +1)
-    }else{
-      setCount (count + 0)
-    }
-    alert(JSON.stringify(values, null, 2));
-    }
-  });
+  },
+];
+const classes = useStyles();
+const theme = useTheme();
+const [activeStep, setActiveStep] = useState(0);
+const maxSteps = tutorialSteps.length;
 
-  async function handleSubmit(e) {
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
-    if (value === "true1"){
-      setCount (count + 1 );
-    } else if (value === "true2") {
-      setCount (count + 1 );
-    }else if (value === "true3"){
-      setCount(count + 1)
-    }else if (value === "true4"){
-      setCount (count +1)
-    }else{
-      setCount (count + 0)
-    }
+const handleBack = () => {
+  setActiveStep((prevActiveStep) => prevActiveStep - 1);
+};
+const handleNext = () => {
+  setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  if (value === "true1"){
+    setCount (count + 1 );
+  } else if (value === "true2") {
+    setCount (count + 1 );
+  }else if (value === "true3"){
+    setCount(count + 1)
+  }else if (value === "true4"){
+    setCount (count +1)
+  }else{
+    setCount (count + 0)
+  }
+
+};
+async function handleSubmit(e) {
+    setOpen(true);
     console.log(formik.values);
     e.preventDefault();
     console.log("Click Firebase");
     try {
       const docRef = await addDoc(collection(db, "Preguntas"), {
-        Pregunta: formik.values.Pregunta,
-        Rta: formik.values.rta
+        1:{
+          rta1:formik.values[1].rta1
+        },
+        2:{
+          rta2:formik.values[2].rta2
+        },
+        3:{
+          rta3:formik.values[3].rta3
+        },
+        4:{
+          rta4:formik.values[4].rta4
+        },
+        score:count
       });
       console.log("Document written with ID: ", docRef.id);
     } catch (e) {
@@ -171,7 +166,7 @@ export const Preguntas = () => {
         variant="text"
         activeStep={activeStep}
         nextButton={
-          <Button size="small" onClick={handleSubmit} disabled={activeStep === maxSteps - 1}>
+          <Button size="small" onClick={handleNext} disabled={activeStep === maxSteps - 1}>
             Next
             {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
           </Button>
@@ -201,7 +196,7 @@ export const Preguntas = () => {
             </IconButton>
           }
           >
-      <h1>Tu puntaje es: {count}</h1>
+      <h1 >Tu puntaje es: {count}</h1>
         </Alert>
       </Collapse>
      
